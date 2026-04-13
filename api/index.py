1d0ba3c6c6dea1c5de794045b8e5416b26782b2c
Karol Medeiros <karol.medeiros@ativuz.com.br>
2026-04-12 21:45:57 -0300

    fix vercel: remove vercel-wsgi

--- api/index.py
+++ api/index.py
@@ -1,11 +1,7 @@
 from flask import Flask
-from vercel_wsgi import handle_request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "API funcionando na Vercel 🚀"
-
-def handler(request):
-    return handle_request(app, request)
